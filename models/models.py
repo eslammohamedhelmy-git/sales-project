90037195005048de0af028b99f0b6991e7badbe9
eslammohamedhelmy-git <70444790+eslammohamedhelmy-git@users.noreply.github.com>
2020-08-30 16:50:18 +0200

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from ebaysdk.poller import orders
 from odoo import models, fields, api
 
 
@@ -10,42 +10,32 @@ class drinks_menu(models.Model):
     name = fields.Char(string="name of drink")
     description = fields.Text(string="description of product")
     price = fields.Float(string="price")
+    price_total = fields.Float(string='total', readonly=True)
+    amount = fields.Float(string="amount")
+    price_total = fields.Float(compute='_compute_amount', string='total', readonly=True, store=True)
     color = fields.Integer()
 
-
-class List_menu(models.Model):
-    _name = 'list.list'
-    _description = 'list_list'
-    _inherit = 'drink.drink'
-
-    name_drinks = fields.Many2one("drink.drink", ondelete="cascade", required=True, index=True,
-                                  string="name of drink")
-    name = fields.Char(related="name_drinks.name", string="name of drink")
-    amount = fields.Float(string="amount", required=True)
-    total_price = fields.Float(string="Total price", store=True, compute='_total_price')
-    Price = fields.Float(related="name_drinks.price", string="price")
-
-    @api.depends('Price', 'amount')
-    def _total_price(self):
-        for r in self:
-            r.total_price = r.Price * r.amount
+    @api.depends('price','amount')
+    def _compute_amount(self):
+        for order in self:
+            order.price_total = order.amount * order.price
 
 
 class order_menu(models.Model):
     _name = 'orders.orders'
     _description = 'orders_orders'
-    _inherit = 'list.list'
+    _inherit = 'drink.drink'
 
-    customer_name = fields.Char(string="customer name", required=True)
-    order_list = fields.Many2many("list.list", 'name_drink', required=True, index=True,
+    customer_name = fields.Many2one("res.partner", ondelet="set null", string="customer name",
+                                    required=True)
+    order_list = fields.Many2many("drink.drink", required=True, index=True,
                                   string="order")
-    order_price = fields.Float(string="order price", store=True, compute='_total_total_price')
-
-    @api.depends('total_price')
-    def _total_total_price(self):
-        x = 0
-        for r in self.order_list:
-            x = x + r.total_price
-            print(x)
-
-
+    # total_price = fields.Float(string="Total price", compute='Total_price', store=True)
+    #
+    # @api.depends('price')
+    # def Total_price(self):
+    #     for r in self:
+    #         total = 0.0
+    #         for line in r.order_list:
+    #             total += line.price
+    #         r.total_price = total
